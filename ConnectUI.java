9f590726b08a1e5cc46ec4c5ac6724b8f06cce4f
Atharva Sabde <67774570+Atharva-Sabde@users.noreply.github.com>
2021-03-20 20:27:16 +0530

    Update ConnectUI.java

--- ConnectUI.java
+++ ConnectUI.java
@@ -1,310 +1,260 @@
 package query_processor_gui;
 
 import java.awt.Color;
-import java.awt.Font;
+import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 
-public class ConnectUI implements ActionListener {
-	JFrame frmcon;
-	JPanel conpanel;
-	JLabel description, db, driver, url, username, password, prt1, prt2, test, label, welcome;
-	public JTextField txtdescription, txtdriver, txturl, txtusername;
-	JPasswordField txtpassword;
-	JComboBox<Object> drpdriver;
-	JButton btnconnect, btncancel, btnreset;
-	Font f1, f2, f3 , f4;
-	ImageIcon icon ;
-
-	String[] strdb = { "Oracle", "MySql",  "HSQL", "Derby", "SQLite","PostGreSql","MariaDb"};
-	String[] strdriver = { 
-			"oracle.jdbc.driver.OracleDriver",
-			"com.mysql.jdbc.Driver" ,
-			"org.hsqldb.jdbc.JDBCDriver" ,
-			"org.apache.derby.jdbc.EmbeddedDriver",
-			"org.sqlite.JDBC",
-			"org.postgresql.Driver",
-			"org.mariadb.jdbc.Driver"								
-	};
-	
-	String[] strurl = { 
-			"jdbc:oracle:thin:@localhost:1521:xe",
-			"jdbc:mysql://localhost:3306/java",
-			"jdbc:hsqldb:file:C:\\\\Users\\\\athar\\\\Downloads\\\\IDEs\\\\DB setups\\\\Hsql",        
-			"jdbc:derby:c:\\derby\\bin\\mydb",
-			"jdbc:sqlite:C:\\Users\\ASUS\\chinook.db",
-			"jdbc:postgresql://localhost/postgres",
-			"jdbc:mysql://localhost:3308/codeheist"
-	};
-
+public class QueryProcessor extends ConnectUI implements ActionListener {
+	JFrame frmquery;
+	JLabel qry , qryresult , qryres ;
+	JTextArea  txtqry , txtqryresult ;
+	JButton btnexecute , btnclear , btnoldqry;
+	ImageIcon execute ;
+	Image image , newimg;
+	String QRY , OLDQRY;
+	static JTable table;
+	static DefaultTableModel tableModel;
 	
 	
-//	String[] strdb = { "Oracle", "MySql",  "HSQL", "Derby", "MS Sql"  };
-//	String[] strdriver = { 
-//			"oracle.jdbc.driver.OracleDriver",
-//			"com.mysql.jdbc.Driver" ,
-//			"org.hsqldb.jdbc.JDBCDriver" ,
-//	};
-//
-//	String[] strurl = { 
-//			"jdbc:oracle:thin:@localhost:1521:xe",
-//			"jdbc:mysql://localhost:3306/--your Table name--",
-//			"jdbc:hsqldb:file:C:\\Users\\athar\\Downloads\\IDEs\\DB setups\\Hsql"
-//	};
-	static String drvIN, urlIN, usrIN, pswIN;
-	
-
-//	String[] strdb = { "Oracle", "MySql", "MS Sql" };
-//	String[] strdriver = { "oracle.jdbc.driver.OracleDriver", "com.mysql.jdbc.Driver" };
-//	String[] strurl = { "jdbc:oracle:thin:@localhost:1521:xe", "jdbc:mysql://localhost:3306/--your Table name--" };
-//	static String drvIN, urlIN, usrIN, pswIN;
-	public ConnectUI() {
-		frmcon = new JFrame();
-		frmcon.setTitle("Connection Prompt...");
-		// frmcon.setLayout(new GridLayout(5,5));
-		frmcon.setVisible(true);
-		frmcon.setSize(800, 500);
-		// frmcon.getContentPane().setBackground(Color.LIGHT_GRAY);
-
+	public QueryProcessor() {
+		super();
+		frmcon.setVisible(false);
+		frmquery = new JFrame("Query Processor");
+//		frmquery.setVisible(true);
+		frmquery.setSize(1200,750);
+		
 		Image ticon = Toolkit.getDefaultToolkit().getImage(
-				"C:\\Users\\athar\\OneDrive\\Desktop\\customization\\Icons\\Numix Circle For Windows\\PNG\\ooo-base.png");
-		frmcon.setIconImage(ticon);
+				"C:\\Users\\athar\\OneDrive\\Desktop\\customization\\Icons\\Numix Circle For Windows\\PNG\\google-sheets.png");
+		frmquery.setIconImage(ticon);
+		execute = new ImageIcon("C:\\\\Users\\\\athar\\\\OneDrive\\\\Desktop\\\\customization\\\\Icons\\\\Numix Circle For Windows\\\\PNG\\\\glc_player.png");
+		image  = execute.getImage(); // transform it 
+		newimg = image.getScaledInstance(80, 80,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+		execute = new ImageIcon(newimg);  // transform it back
+		
+		
+		//Labels
+		qry= new JLabel("----------------------------  Enter Query:  ----------------------------");
+		qry.setBounds(150,10,500,30);
+		qry.setFont(f4);
+//		qryresult = new JLabel("");
+//		qryresult.setBounds(150, 150,500, 30);
+//		qryresult.setFont(f4);
+		
+		qryres = new JLabel("------------------------  Query Output:  ---------------------------------------------------------------------------------------------------------");
+		qryres.setBounds(150, 150,500, 30);
+		qryres.setFont(f4);
+		frmquery.add(qryres);
+		//TExtarea
+		txtqry= new JTextArea();
+		txtqry.setBounds(30,50, 600, 80);
+		txtqry.setBackground(Color.black);
+		txtqry.setForeground(Color.green);
+		txtqry.setFont(f4);
+		frmquery.add(txtqry);
+		
+		txtqryresult = new JTextArea();
+		txtqryresult.setBounds(30,250, 730, 170);
+		txtqryresult.setFont(f4);
+	//	frmquery.add(txtqryresult);
+		
 
-		f1 = new Font("Arial", Font.BOLD, 20);
-		f2 = new Font("Arial", Font.BOLD, 16);
-		f3 = new Font("Arial", Font.ITALIC, 14);
-		f4 = new Font("Sans", Font.TYPE1_FONT, 16);
+		//Buttons
+		btnexecute = new JButton();  //"Execute"
+		btnexecute.setIcon(execute);
+		btnexecute.setBounds(650, 50, 80,80);
+		//btnexecute.setBackground(Color.green);
+		frmquery.add(btnexecute);
 
-		// welcome page
-		icon = new ImageIcon(
-				"C:\\Users\\athar\\OneDrive\\Desktop\\customization\\Icons\\Numix Circle For Windows\\PNG\\oracle-sqldeveloper.png");
-		welcome = new JLabel("We <3 Databases!");
-		welcome.setBounds(310, 50, 500, 30);
-		welcome.setFont(f1);
-		// welcome.setForeground(Color.BLACK);
-		frmcon.add(welcome);
-		label = new JLabel(icon);
-		label.setSize(800, 500);
-		// label.setBounds(x, y, width, height);
-		label.setVisible(true);
-		frmcon.add(label);
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		label.setVisible(false);
-		welcome.setVisible(false);
-
-		// frmcon.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// frmcon.pack();
-		// frmcon.setForeground(Color.green);
-
-		// ComboBox
-
-		drpdriver = new JComboBox<Object>(strdb);
-		drpdriver.setBounds(390, 85, 80, 30);
-		drpdriver.setFont(f2);
-
-		// drpdriver.setEditable(true);
-		// drpdriver.addActionListener(this);
-		// drpdriver.setBounds(x, y, width, height);
-
-		// description = new JLabel("Enter your DataBase Details...");
-		description = new JLabel("Database Query Processor");
-		description.setBounds(260, 5, 500, 30);
-		// description.setHorizontalAlignment(description.LEFT);
-		description.setFont(f1);
-		description.setForeground(Color.RED);
-
-		prt1 = new JLabel(
-				"----------------------------------------   DRIVER INFO   ----------------------------------------");
-		prt1.setBounds(100, 50, 600, 20);
-		prt1.setHorizontalAlignment(prt1.CENTER);
-		prt1.setFont(f3);
-		prt2 = new JLabel(
-				"-------------------------------------   AUTHENTICATION   -------------------------------------");
-		prt2.setBounds(100, 255, 600, 20);
-		prt2.setHorizontalAlignment(prt2.CENTER);
-		prt2.setFont(f3);
-
-		// LABELS
-		db = new JLabel("Database:");
-		db.setBounds(300, 85, 150, 30);
-		db.setFont(f2);
-
-		driver = new JLabel("Driver     :");
-		driver.setBounds(100, 120, 100, 30);
-		driver.setFont(f2);
-
-		url = new JLabel("URL        :");
-		url.setBounds(100, 160, 100, 30);
-		url.setFont(f2);
-
-		username = new JLabel("Username   :");
-		username.setBounds(220, 290, 100, 30);
-		username.setFont(f2);
-
-		password = new JLabel("Password   :");
-		password.setBounds(220, 330, 100, 30);
-		password.setFont(f2);
-
-		// TEXTFIELDS
-		txtdriver = new JTextField();
-		txtdriver.setFont(f2);
-		txtdriver.setBounds(180, 120, 500, 30);
-
-		txturl = new JTextField();
-		txturl.setFont(f2);
-		txturl.setBounds(180, 160, 500, 30);
-
-		txtusername = new JTextField();
-		txtusername.setBounds(325, 290, 250, 30);
-		txtusername.setFont(f2);
-//txtusername.setText("system");
-		txtpassword = new JPasswordField();
-		txtpassword.setBounds(325, 330, 250, 30);
-		txtpassword.setFont(f2);
-//txtpassword.setText("2903");
-
-		// BUTTONS
-		btnconnect = new JButton("Connect");
-		btnconnect.setFont(f2);
-		btnconnect.setBounds(250, 390, 130, 40);
-		btnconnect.setBackground(Color.CYAN);
-
-		btnreset = new JButton("Reset");
-		btnreset.setFont(f2);
-		// btnreset.setBounds(360, 390,120,40);
-		btnreset.setBounds(330, 200, 130, 30);
-		btnreset.setBackground(Color.orange);
-
-		btncancel = new JButton("Cancel");
-		btncancel.setBounds(420, 390, 120, 40);
-		btncancel.setFont(f2);
-		btncancel.setBackground(Color.PINK);
-
-		// adding
-		frmcon.add(btnconnect);
-		frmcon.add(btnreset);
-		frmcon.add(btncancel);
-
-		frmcon.add(description);
-		frmcon.add(prt1);
-		frmcon.add(prt2);
-
-		frmcon.add(db);
-		frmcon.add(driver);
-		frmcon.add(url);
-		frmcon.add(username);
-		frmcon.add(password);
-
-		frmcon.add(drpdriver);
-		frmcon.add(txtdriver);
-		frmcon.add(txturl);
-		frmcon.add(txtusername);
-		frmcon.add(txtpassword);
-		frmcon.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-		test = new JLabel();
-		test.setBounds(10, 10, 100, 30);
-		//test.setText(url);
-		frmcon.add(test);
-
-		frmcon.setVisible(true);
-
-		drpdriver.addActionListener(this);
-		btnreset.addActionListener(this);
-		btnconnect.addActionListener(this);
-		btncancel.addActionListener(this);
-
-	}
-
+		btnclear = new JButton("CLEAR");
+		btnclear.setBounds(30, 150, 600,30);
+		//btnclear.setBackground(Color.YELLOW);
+		//btnclear.setOpaque(false);
+		frmquery.add(btnclear);
+		
+		btnoldqry = new JButton("Previous");
+		btnoldqry.setBounds(650, 150, 80,30);
+		btnoldqry.setBackground(Color.cyan);
+		frmquery.add(btnoldqry);
+		
+		//Adding..
+		frmquery.add(qry);
+//		frmquery.add(qryresult);
+		frmquery.add(qryres);
+		
+		
+	
+		
+		frmquery.setVisible(true);
+		frmquery.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+		btnoldqry.addActionListener(this);
+		btnexecute.addActionListener(this);
+		btnclear.addActionListener(this);
+		
+	}// constructor closed
 //---------------------------------------------------------------------------------------------------------------------------
-
-	public boolean connect(String DRIVER, String URL, String USR, String PSW) {
-		try{
-			Class.forName(DRIVER);
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent aee) {
+		Object action = aee.getSource();
+		QRY = txtqry.getText();
+		//String qry, result ;
+		if(action == btnexecute) {
 			
-			Connection conn = DriverManager.getConnection(URL, USR, PSW);
+		//	System.out.println(OLDQRY);
+		//	qry.toLowerCase();
+			//if(qry.contains("insert")) {	
+			try {
+//				if(QRY.contains("*")) {
+//					//add dql status button if possible
+//					//table.setVisible(true);
+//					txtqryresult.setVisible(false);
+					fetchTable(QRY);
+//				}else {
+//					//table.setVisible(false);
+//					txtqryresult.setVisible(true);
+//					fetch(QRY);
+//				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		else if(action == btnoldqry) {
+			txtqry.setText(OLDQRY);
+			btnoldqry.setBackground(Color.yellow);
+		}
+		else if(action == btnclear) {
+			txtqry.setText(null);
+			txtqryresult.setText(null);
+		}
+		
+	}// Action close
+	
+//=========================================== Logic MEthods========================================
+	
+	@SuppressWarnings("deprecation")
+	public  boolean fetchTable(String QRY) throws ClassNotFoundException{
+		try {
+			Class.forName(drvIN);
+			System.out.println(drvIN);
+			Connection conn = DriverManager.getConnection(urlIN, usrIN, pswIN);
+			System.err.println("Connected");
+//			table.setAutoscrolls(true);
+			
+			
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(QRY);
+
+			ResultSetMetaData rsmd = rs.getMetaData();
+			
+			
+			tableModel = new DefaultTableModel();
+			int colNo = rsmd.getColumnCount();
+			Object[] row = new Object[colNo];
+			while(rs.next()) {
+				for (int i=0 ; i<colNo ; i++) {
+					int x = i+1;
+					row[i] = rs.getObject(x);
+				}
+				tableModel.addRow(row);
+			}
+
+	
+			table = new JTable();
+			JTableHeader header = table.getTableHeader();
+			header.setBackground(Color.green);
+			header.setBackground(Color.black);
+			
+			table.setModel(tableModel);
+			table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+			table.setFillsViewportHeight(true);
+			
+			
+			JScrollPane jsp = new JScrollPane(table);
+			jsp.setBounds(10,300,1000,500);
+			jsp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+			jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+			
+			for (int   i=1 ; i<=colNo  ;  i++) {
+				tableModel.addColumn(rsmd.getColumnLabel(i));
+			}
+
+//			table.setPreferredScrollableViewportSize();
+//			table.setBounds(20, 250, 750, 500);
+			
+			frmquery.add(jsp);
+			rs.close();
 			conn.close();
 			return true;
 
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (SQLException se) {
+			// TODO Auto-generated catch block
+			se.printStackTrace();
+//			txtqryresult.setText(se);
 			return false;
 		}
-
-	}// method close
-//---------------------------------------------------------------------------------------------------------------------------
+		
+		
+	}
 	
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public void actionPerformed(ActionEvent ae) {
-		
-		Object action = ae.getSource();
-		if (action == drpdriver) {
-
-			// Object dbselect = drpdriver.getSelectedItem();
-			int dbindex = drpdriver.getSelectedIndex();
-
-			txtdriver.setText(strdriver[dbindex]);
-			txturl.setText(strurl[dbindex]);
-		}
-		drvIN = txtdriver.getText();
-		urlIN = txturl.getText();
-		usrIN = txtusername.getText();
-		pswIN = txtpassword.getText();
-		
-		
-		if (action == btnconnect) {
+	//Only for singular queries....
+	private void fetch(String qRY2) throws ClassNotFoundException {
+		try {
+			Class.forName(drvIN);
+			System.out.println(drvIN);
+			Connection conn = DriverManager.getConnection(urlIN, usrIN, pswIN);
 			
-			boolean valid;
-			valid = connect(drvIN, urlIN, usrIN, pswIN);
-			if(valid) {
-				btnconnect.setBackground(Color.green);
-				frmcon.getContentPane().setBackground(Color.LIGHT_GRAY);;
-				JOptionPane.showMessageDialog(frmcon, "Database Connected!");
-				frmcon.setVisible(false);
-				QueryProcessor qp = new QueryProcessor();
-			} 
-			else {
-				btnconnect.setBackground(Color.red);
-				JOptionPane.showMessageDialog(frmcon, "Database Connection Failed");
-			}
-		} 
-		
-		else if (action == btnreset) {
-			btnreset.setBackground(Color.green);
-			txtdriver.setText(null);
-			txturl.setText(null);
-			txtusername.setText(null);
-			txtpassword.setText(null);
-		} 
-		
-		else if (action == btncancel) {
-			// frmcon.setDefaultCloseOperation(1);
-			frmcon.dispose();
-			// frmcon.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(QRY);
+			 
+			rs.next();
+			String out = rs.getString(1);
+			System.out.println(out);
+			txtqryresult.setVisible(true);
+			txtqryresult.setText(out);
+			
+			conn.close();
+			
+		}catch (SQLException se) {
+			// TODO Auto-generated catch block
+			se.printStackTrace();
+//			txtqryresult.setText(se);
+	
 		}
-	}// action closed
+		
+	}
 
-}// ConnectUI closed
+	//=========================MAIN===============================
+	public static void main(String[] args) {
+		ConnectUI cnt = new ConnectUI();
+//		QueryProcessor qp = new QueryProcessor();
+	}
+}
